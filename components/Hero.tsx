85c0910de9e2b57d03a7b549d51eb1083f0104cd
motion-edit <elainewan728@gmail.com>
2025-12-10 16:15:03 -0800

    Updated video

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -127,7 +127,7 @@ const Hero: React.FC = () => {
                loop
                playsInline
              >
-                <source src="motionedit_images/motionedit_v2.mp4" type="video/mp4" />
+                <source src="motionedit_images/motionedit_v3.mp4" type="video/mp4" />
                 <div className="w-full h-full flex items-center justify-center text-white/50">
                   Video Placeholder
                 </div>
